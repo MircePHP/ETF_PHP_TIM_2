79f9aac5ebc1a2a35c5b3eb33b74c0bf267d9521
Mirjana Markovic <mirjanamarkovic1517@gmail.com>
2021-09-07 23:11:48 +0200

    uneti tagovi za bootstrap

--- app/Views/default.php
+++ app/Views/default.php
@@ -8,6 +8,16 @@
 	<link rel="shortcut icon" type="image/png" href="/favicon.ico"/>
 
 	<!-- STYLES -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+<!-- Popper JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
 	<style {csp-style-nonce}>
 		* {
@@ -227,6 +237,11 @@
 
 <!-- CONTENT -->
 
+<div class="container">
+
+<?= $this->renderSection('content') ?>
+
+</div>
 
 <!-- FOOTER: DEBUG INFO + COPYRIGHTS -->
 
@@ -247,6 +262,8 @@
 	</div>
 
 </footer>
+
+<?= $this->renderSection('sidebar') ?>
 
 <!-- SCRIPTS -->
 
